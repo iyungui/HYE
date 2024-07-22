4128c2d4c68a4421eb6fd7fc32d9e27e54cca2d2
iyungui <rsdbddml@gmail.com>
2024-07-22 22:37:54 +0900

    update gitignore

---
+++ HWYU/View/EggView.swift
@@ -0,0 +1,18 @@
+//
+//  EggView.swift
+//  HWYU
+//
+//  Created by Yungui Lee on 7/18/24.
+//
+
+import SwiftUI
+
+struct EggView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EggView()
+}
