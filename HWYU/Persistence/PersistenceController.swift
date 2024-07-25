3bc5e3b786f7df1bd61a2db867af632c0199aa44
iyungui <rsdbddml@gmail.com>
2024-07-25 22:34:08 +0900

    update persistenceController

--- HWYU/Persistence/PersistenceController.swift
+++ HWYU/Persistence/PersistenceController.swift
@@ -10,6 +10,17 @@ import CoreData
 class PersistenceController {
     static let shared = NSPersistentContainer()
     
+    let container: NSPersistentCloudKitContainer
+    
+    init() {
+        container = NSPersistentCloudKitContainer(name: "ShareLogs")
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Container load failed: \(error)")
+            }
+        }
+        container.viewContext.automaticallyMergesChangesFromParent = true
+    }
 }
 
 /*
