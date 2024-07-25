19aa65803d7e209e77a8e1a16576e37632037041
iyungui <rsdbddml@gmail.com>
2024-07-25 21:55:57 +0900

    Firebase 삭제

--- HWYU/HWYUApp.swift
+++ HWYU/HWYUApp.swift
@@ -6,20 +6,9 @@
 //
 
 import SwiftUI
-import Firebase
-import FirebaseCore
-import FirebaseFirestore
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-       FirebaseApp.configure()
-       return true
-   }
-}
 
 @main
 struct HWYUApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     var body: some Scene {
         WindowGroup {
